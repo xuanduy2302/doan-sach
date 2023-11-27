f260f92c005961a93ef935f804fd74158f7eec4e
camdao <luongcamdao@gmail.com>
2023-11-27 20:03:59 +0700

    a

---
+++ SACH/QuanLyDSSach.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class QuanLyDSSach{
+    public DSSach dsSach = new DSSach();
+    Scanner sc=new Scanner(System.in);
+
+    public void Menu(){
+        int lua_chon=0;
+        boolean flag = true;
+        while(flag){
+            System.out.print(" 1)Them sach\n 2)Xoa sach\n 3)Sua sach\n 4)Xuat danh sach sach\n 5)Tim kiem sach\n 6)Kho sach\n 7)Quay lai\n 8)Thoat\n");
+            System.out.print("Lua chon: ");
+            lua_chon=sc.nextInt();
+            switch(lua_chon){
+                case 1:
+                    dsSach.them();
+                    break;
+                case 2:
+                    dsSach.xoa();
+                    break;
+                case 3:
+                    dsSach.sua();
+                    break;
+                case 4:
+                    dsSach.xuat();
+                    break;
+                case 5:
+                    dsSach.timkiem();
+                    break;
+                case 6:
+                    System.out.println("So luong sach co trong kho: "+SACH.getKhoSach());
+                case 7:
+                    flag = false;
+                    break;
+                case 8:
+                    System.exit(0);
+
+            }
+        }
+    }
+}
---
+++ SACH/SACH.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class SACH{
+    Scanner sc = new Scanner(System.in);
+    
+    public String MaSach;
+    public String TenSach;
+    public String NXB;
+    public String TacGia;
+    public int SoTrang;
+    public int SoLuong;
+    public int GiaTien;
+
+    private static int KhoSach;
+    public static int getKhoSach(){
+        return KhoSach;
+    }
+    public static void updateKhoSach(int n){//n la so luong sach muon them hoac giam 
+        KhoSach+=n;
+    }
+    public SACH(){
+        MaSach=NXB=TacGia="";
+        SoLuong=SoTrang=GiaTien=0;
+    }
+    public SACH(String MaSach,String TenSach,String NXB,String TacGia,int SoTrang,int SoLuong,int GiaTien){
+        MaSach = this.MaSach;
+        TenSach = this.TenSach;
+        NXB = this.NXB;
+        TacGia = this.TacGia;
+        SoTrang = this.SoTrang;
+        SoLuong = this.SoLuong; 
+        KhoSach+=SoLuong;
+        GiaTien = this.GiaTien;
+    }
+    public void xuat(){
+        System.out.print("Ma sach: "+MaSach+", Ten sach: "+TenSach+", Nha xuat ban: "+NXB+", Tac gia: "+TacGia+", So Trang: "+SoTrang+", So luong:"+SoLuong+", Gia Tien: "+GiaTien);
+    }
+    public void nhap(){
+        System.out.println("Nhap cac thong tin cua sach: ");
+        System.out.print("Ma sach: "); MaSach=sc.nextLine();
+        System.out.print("Ten sach: "); TenSach=sc.nextLine();
+        System.out.print("Nha xuat ban: "); NXB=sc.nextLine();
+        System.out.print("Tac gia: "); TacGia=sc.nextLine();
+        System.out.print("So trang: "); SoTrang=sc.nextInt();
+        System.out.print("So luong: "); SoLuong=sc.nextInt(); 
+        KhoSach+=SoLuong;
+        System.out.print("Gia tien: "); GiaTien=sc.nextInt();
+    }
+
+    @Override
+    public String toString() {
+        return MaSach + "|" + TenSach + "|" + NXB+ "|" + TacGia + "|" + SoTrang+ "|" + SoLuong + "|" + GiaTien;
+    }
+}
+
