455a7e856f6242d62aea9c68787075a5c533fed7
xuanduy2302 <152255834+xuanduy2302@users.noreply.github.com>
2023-11-28 14:01:05 +0700

    Update DSNhanVien.java

--- doansach/src/NhanVien/DSNhanVien.java
+++ doansach/src/NhanVien/DSNhanVien.java
@@ -1,13 +1,16 @@
-package NHANVIEN;
-
+package NhanVien;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Scanner;
-import abstr_interf.*;
 
-public class DSNhanVien extends MENU{
-	public static ArrayList<NHANVIEN> DanhSachNhanVien;
+
+class DSNhanVien extends Menu{
+	public static ArrayList<NhanVien> DSNhanVien;
 	public DSNhanVien() {
-		DanhSachNhanVien = new ArrayList<NHANVIEN>(0);
+		DSNhanVien = new ArrayList<NhanVien>(0);
 	}
     Scanner sc = new Scanner(System.in);
     
@@ -15,28 +18,28 @@ public class DSNhanVien extends MENU{
     	String choose;
     	System.out.println("Nhap cong viec cua nhan vien: \n1.Thu ngan. \n2.Kho. \n3.Bao ve."); choose=sc.nextLine();
 		if(choose.equals("1")) {
-			NHANVIEN x=new THUNGAN();
+			NhanVien x=new ThuNgan();
 			x.nhap();
-			DanhSachNhanVien.add(x);
+			DSNhanVien.add(x);
 		}
 		else if(choose.equals("2")) {
-			NHANVIEN x=new KHO();
+			NhanVien x=new Kho();
 			x.nhap();
-			DanhSachNhanVien.add(x);
+			DSNhanVien.add(x);
 		}
 		else if(choose.equals("3")) {
-			NHANVIEN x=new BAOVE();
+			NhanVien x=new BaoVe();
 			x.nhap();
-			DanhSachNhanVien.add(x);
+			DSNhanVien.add(x);
 		}
     }
     public void xoa(){
         String MaNVXoa;
         boolean found=false;
         System.out.print("Nhap ma nhan vien muon xoa: "); MaNVXoa=sc.nextLine();
-        for (NHANVIEN NhanVien : DanhSachNhanVien)
+        for (NhanVien NhanVien : DSNhanVien)
             if (NhanVien.manv.equals(MaNVXoa)){
-                DanhSachNhanVien.remove(NhanVien);
+                DSNhanVien.remove(NhanVien);
                 found = true;
                 System.out.println("Da xoa !");
                 break;
@@ -50,9 +53,10 @@ public class DSNhanVien extends MENU{
         System.out.print("Nhap ma nhan vien muon sua: "); MaNVSua=sc.nextLine();
         System.out.println("Chon thong tin can sua: ");
         
-        for (NHANVIEN NhanVien : DanhSachNhanVien){
+        for (NhanVien NhanVien : DSNhanVien){
             if (NhanVien.manv.equals(MaNVSua)){
                 found = true;
+                Scanner sc=new Scanner(System.in);
 				int sua;
 				System.out.println("Chon muc can sua: \n1.Ma nhan vien. \n2.Ten nhan vien. \n3.Ngay sinh nhan vien. \n4.SDT nhan vien. \n5.Dia chi. ");
 				sua=sc.nextInt();
@@ -84,8 +88,8 @@ public class DSNhanVien extends MENU{
     }
     public void danhsach(){
         int i=1;
-        System.out.println("Danh sach co "+DanhSachNhanVien.size()+" nhan vien:");
-        for (NHANVIEN nv : DanhSachNhanVien) {
+        System.out.println("Danh sach co "+DSNhanVien.size()+" nhan vien:");
+        for (NhanVien nv : DSNhanVien) {
             System.out.print(i+"/ ");
             nv.xuat();
             System.out.println();
@@ -96,7 +100,7 @@ public class DSNhanVien extends MENU{
         String MaNVTimKiem;
         boolean found=false;
         System.out.print("Nhap ma nhan vien can tim: "); MaNVTimKiem=sc.nextLine();
-        for (NHANVIEN NhanVien : DanhSachNhanVien){
+        for (NhanVien NhanVien : DSNhanVien){
             if (NhanVien.manv.equals(MaNVTimKiem)){
                 found = true;
                 NhanVien.xuat();
@@ -106,4 +110,89 @@ public class DSNhanVien extends MENU{
         if (!found)
             System.out.println("Khong tim thay nhan vien !");
     }
+    public void WriteFile() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("datanhanvien.txt"))) {
+            for (NhanVien x : DSNhanVien) {
+                writer.write(x.manv + ",");
+                writer.write(x.tennv + ",");
+                writer.write(x.ngaysinhnv + ",");
+                writer.write(x.sdtnv + ",");
+                writer.write(x.diachinv + ",");
+                writer.write(x.luong + ",");
+                if(x instanceof ThuNgan) {
+                	writer.write("1,");
+                	writer.write( ((ThuNgan)x).ngaybanhang + ",");
+                }
+                if(x instanceof Kho) {
+                	writer.write("2,");
+                	writer.write( ((Kho)x).ngaykiemkho + ",");
+                }
+                if(x instanceof BaoVe) {
+                	writer.write("3,");
+                	writer.write( ((BaoVe)x).catruc + ",");
+                }
+                writer.newLine(); // Xuống dòng cho mỗi nhân viên
+                
+            }
+            System.out.println("Ghi file thanh cong!");
+     
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void ReadFile() {
+        try (BufferedReader reader = new BufferedReader(new FileReader("datanhanvien.txt"))) {
+        	String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                String maNV = parts[0];
+                String tenNV = parts[1];
+                String ngaySinhNV = parts[2];
+                String sdtNV = parts[3];
+                String diaChiNV = parts[4];
+                String luongNV = parts[5];
+                String currentRole = parts[6];
+                NhanVien nv ;
+                if (currentRole.equals("1")) {
+                	nv = new ThuNgan();
+                    ((ThuNgan)nv).ngaybanhang = parts[7];
+                    nv.manv = maNV;
+                    nv.tennv = tenNV;
+                    nv.ngaysinhnv = ngaySinhNV;
+                    nv.sdtnv = sdtNV;
+                    nv.diachinv = diaChiNV;
+                    nv.luong = luongNV;
+                   
+                    DSNhanVien.add(nv);
+                } else if (currentRole.equals("2")) {
+                    nv = new Kho();
+                    ((Kho)nv).ngaykiemkho = parts[7]; 
+                    nv.manv = maNV;
+                    nv.tennv = tenNV;
+                    nv.ngaysinhnv = ngaySinhNV;
+                    nv.sdtnv = sdtNV;
+                    nv.diachinv = diaChiNV;
+                    nv.luong = luongNV;
+                   
+                    DSNhanVien.add(nv);
+                } else if (currentRole.equals("3")) {
+                    nv = new BaoVe();
+                    ((BaoVe)nv).catruc = parts[7];
+                    nv.manv = maNV;
+                    nv.tennv = tenNV;
+                    nv.ngaysinhnv = ngaySinhNV;
+                    nv.sdtnv = sdtNV;
+                    nv.diachinv = diaChiNV;
+                    nv.luong = luongNV;
+                   
+                    DSNhanVien.add(nv);
+                }            
+            }   
+            System.out.println("Doc file thanh cong!");
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
+ 
