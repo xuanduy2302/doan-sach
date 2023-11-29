5f4708b26f28b2f3e889eccbee37bd3de5332295
HTD <dhuynhdp@gmail.com>
2023-11-29 14:28:00 +0700

    them rang buoc cho khach hang

--- DoAn/src/KHACH_HANG/QL_DSKHACHHANG.java
+++ DoAn/src/KHACH_HANG/QL_DSKHACHHANG.java
@@ -10,52 +10,47 @@ public class QL_DSKHACHHANG {
     public void Menu(){
         int choose=0;
         boolean flag1=true;
-            DSKHACHHANG.doc();//đọc file mỗi khi chạy hàm select 1 lần
-            while(flag1){//menu
-                boolean flag2=true;//lưu ý flag2 ở trong này mới đúng chứ ở ngoài khai báo với flag1 là bị loop ở x.them();
-                System.out.println("\nCHON THAO TAC:\n1 Them moi\n2 Sua thong tin\n3 Tim kiem\n4 Xoa\n5 Hien thi danh sach\n6 Thoat");    
-                while(flag2){
-                    if(sc.hasNextInt()){
-                        choose=sc.nextInt();
-                    if(choose<=6 && choose>0)
-                            flag2=false;
-                        else{
-                            System.out.println("nhap sai thong tin");
-                            System.out.println("\nCHON THAO TAC:\n1 Them moi\n2 Sua thong tin\n3 Tim kiem\n4 Xoa\n5 Hien thi danh sach\n 6 Quay lai\n 7 Thoat");
-                        }     
-                    }
+        while(flag1){//menu
+            boolean flag2=true;//lưu ý flag2 ở trong này mới đúng chứ ở ngoài khai báo với flag1 là bị loop ở x.them();
+            System.out.println("\nCHON THAO TAC:\n1:Them moi\n2:Sua thong tin\n3:Tim kiem\n4:Xoa\n5:Hien thi danh sach\n6:Thoat");    
+            while(flag2){
+                if(sc.hasNextInt()){
+                    choose=sc.nextInt();
+                    sc.nextLine();
+                if(choose<=6 && choose>0)
+                        flag2=false;
                     else{
-                        System.out.println("vui long nhap so nguyen");
-                        sc.next();
-                    }
+                        System.out.println("!!! Nhap sai thong tin");
+                        System.out.println("\nCHON THAO TAC:\n1:Them moi\n2:Sua thong tin\n3:Tim kiem\n4:Xoa\n5:Hien thi danh sach\n 6:Thoat");
+                    }     
                 }
-                sc.nextLine();
-        
-                switch (choose) {
-                    case 1:
-                        x.them();
-                        break;
-                    case 2:
-                        x.sua();
-                        break;
-                    case 3:
-                        x.timkiem();
-                        break;
-                    case 4:
-                        x.xoa();
-                        break;
-                    case 5:
-                        x.danhsach();
-                        break;
-                    case 6:
-                        flag1=false;
-                        //static nên gọi class và tên hàm khi thực hiện
-                        DSKHACHHANG.luu();//lưu file sau khi thoát chương trình
-                        break;
-                    case 7:
-                        DSKHACHHANG.luu();//lưu file sau khi thoát chương trình
-                        System.exit(0); 
-                }       
+                else{
+                    System.out.println("!!! Vui long nhap so nguyen");
+                    sc.next();
+                }
             }
+            switch (choose){
+                case 1:
+                    x.them();
+                    break;
+                case 2:
+                    x.sua();
+                    break;
+                case 3:
+                    x.timkiem();
+                    break;
+                case 4:
+                    x.xoa();
+                    break;
+                case 5:
+                    x.danhsach();
+                    break;
+                case 6:
+                    flag1=false;
+                    //static nên gọi class và tên hàm khi thực hiện
+                    DSKHACHHANG.luu();//lưu file sau khi thoát chương trình
+                    break;
+            }       
+        }
     }
 }
